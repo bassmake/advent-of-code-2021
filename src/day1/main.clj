3398dfc704e166ef2d615bc267304f929ea69b1e
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2021-12-07 18:19:29 +0100

    day1 part2 solved

--- src/day1/main.clj
+++ src/day1/main.clj
@@ -15,6 +15,25 @@
                              (xf result [prior input]))))))))
   ([coll] (sequence (make-pairs) coll)))
 
+(defn append-to-pair [pair to-add]
+  (cond
+    (empty? pair) [to-add]
+    :else [(last pair) to-add]))
+
+(defn make-triplets ([]
+                     (fn [xf]
+                       (let [prev (volatile! [])]
+                         (fn
+                           ([] (xf))
+                           ([result] (xf result))
+                           ([result input]
+                            (let [prior @prev]
+                              (vreset! prev (append-to-pair prior input))
+                              (if (< (count prior) 2)
+                                []
+                                (xf result (conj prior input)))))))))
+  ([coll] (sequence (make-triplets) coll)))
+
 (defn compare-change [[first second]]
   (cond
     (> first second) ::decreased
@@ -22,7 +41,7 @@
     (= first second) ::same
     :else ::unknown))
 
-(defn provide-solution [file]
+(defn provide-solution-1 [file]
   (with-open
    [rdr (io/reader (str "src/day1/" file))]
     (let [changes (->> (line-seq rdr)
@@ -31,11 +50,37 @@
                        (map compare-change))]
       (count (filter #(= ::increased %) changes)))))
 
-(provide-solution "input.txt")
+
+(defn provide-solution-2 [file]
+  (with-open
+   [rdr (io/reader (str "src/day1/" file))]
+    (let [changes (->> (line-seq rdr)
+                       (map #(Integer/parseInt %))
+                       (make-triplets)
+                       (map #(reduce + %))
+                       (make-pairs)
+                       (map compare-change))]
+      (count (filter #(= ::increased %) changes)))))
+
+
+(provide-solution-1 "input.txt")
+(provide-solution-2 "input.txt")
+
 
 (comment
   (make-pairs [1 2 3 4])
+
+  (append-to-pair [] 2)
+  (append-to-pair [1] 2)
+  (append-to-pair [3 4] 1)
+  (append-to-pair [3 4 9] 1)
+
+  (make-triplets [1 2 3 4 5 6])
+
   (compare-change [2 3])
   (compare-change [2])
   (compare-change [])
-  (provide-solution "input-sample.txt"))
+
+
+  (provide-solution-1 "input-sample.txt")
+  (provide-solution-2 "input-sample.txt"))
