8197e83d59dc67cc55a6cfb784d2fb17c100b459
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2021-12-07 21:09:45 +0100

    day2 part2

--- src/day2/main.clj
+++ src/day2/main.clj
@@ -14,17 +14,31 @@
       (= direction "down") {:horizontal horizontal :depth (+ depth units)}
       :else (throw (AssertionError. (str "Wrong input." position command))))))
 
+(defn aim-move [position command]
+  (let [{horizontal :horizontal depth :depth aim :aim} position {direction :direction units :units} command]
+    (cond
+      (= direction "forward") {:horizontal (+ horizontal units) :depth (+ depth (* aim units)) :aim aim}
+      (= direction "up") {:horizontal horizontal :depth depth :aim (- aim units)}
+      (= direction "down") {:horizontal horizontal :depth depth :aim (+ aim units)}
+      :else (throw (AssertionError. (str "Wrong input." position command))))))
+
 (defn solution1 [lines]
   (reduce move {:horizontal 0 :depth 0} (map parse lines)))
 
-(defn provide-solution1 [file]
-  (let [{horizontal :horizontal depth :depth} (tools/with-open-file (str "src/day2/" file) solution1)]
+(defn solution2 [lines]
+  (reduce aim-move {:horizontal 0 :depth 0 :aim 0} (map parse lines)))
+
+(defn provide-solution [solution file]
+  (let [{horizontal :horizontal depth :depth} (tools/with-open-file (str "src/day2/" file) solution)]
     (* horizontal depth)))
 
 (comment
-  (provide-solution1 "input-sample.txt")
+  (= (provide-solution solution1 "input-sample.txt") 150)
+  (= (provide-solution solution1 "input.txt") 2073315)
 
-  (provide-solution1 "input.txt")
+  (= (provide-solution solution2 "input-sample.txt") 900)
+  (= (provide-solution solution2 "input.txt") 1840311528)
+
 
   (move {:horizontal 1 :depth 3} {:direction "down" :units 23})
 
