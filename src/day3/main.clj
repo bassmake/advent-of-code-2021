70ba3f6883188a25d8438fd64f1aa58dd7cfcb47
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2021-12-08 19:18:56 +0100

    day 3 part 1

--- src/day3/main.clj
+++ src/day3/main.clj
@@ -1,25 +1,52 @@
 (ns day3.main
-  (:require [tools.main :as tools]))
+  (:require [tools.main :as tools]
+            [clojure.math.numeric-tower :as math]))
 
 (defn parse [line]
   (map #(= \1 %) (seq (char-array line))))
 
-(defn add-line [counter line]
-  
-  )
+(defn sum [total line]
+  (let [pairs (map vector (concat total (repeat 0)) line)]
+    (map (fn [[value bit]] (if (true? bit) (inc value) value)) pairs)))
+    ;; pairs))
 
-(defn count-ones [lines]
-  (reduce add-line (map parse lines)))
+(defn increase-counter [counter line]
+  (let [total (inc (:total counter))
+        sum (sum (:sum counter) line)]
+    {:total total :sum sum}))
 
 (defn solution [lines]
-  (println lines))
+  (reduce increase-counter {:total 0 :sum []} (map parse lines)))
+
+(defn from-binary [binary]
+  (let [powers (map #(math/expt 2 %) (range))]
+    (->> binary
+         (reverse)
+         (map vector powers)
+         (map #(if (true? (last %)) (first %) 0))
+         (reduce +))))
+    ;; powers))
+
+;; pairs (map vector binary powers)
+;; numbers (map)
 
 (defn provide-solution [solution file]
-  (tools/with-open-file (str "src/day3/" file) solution))
+  (let [{total :total sum :sum} (tools/with-open-file (str "src/day3/" file) solution)
+        half (/ total 2)
+        gamma_bin (map #(> half %) sum)
+        epsilon_bin (map #(< half %) sum)
+        gamma (from-binary gamma_bin)
+        epsilon (from-binary epsilon_bin)]
+    {:gamma gamma :epsilon epsilon :solution (* gamma epsilon)}))
 
 (comment
 
   (parse "1010001")
 
+  (from-binary (parse "01001"))
+
+  (sum [1 2 3 4] (parse "1010001"))
+
   (provide-solution solution "input-sample.txt")
+
   (provide-solution solution "input.txt"))
