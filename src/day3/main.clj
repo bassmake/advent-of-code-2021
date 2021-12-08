791d9b7ead792dd5f4b9c000d4297be86395fc2e
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2021-12-08 19:24:40 +0100

    small refactoring

--- src/day3/main.clj
+++ src/day3/main.clj
@@ -5,19 +5,6 @@
 (defn parse [line]
   (map #(= \1 %) (seq (char-array line))))
 
-(defn sum [total line]
-  (let [pairs (map vector (concat total (repeat 0)) line)]
-    (map (fn [[value bit]] (if (true? bit) (inc value) value)) pairs)))
-    ;; pairs))
-
-(defn increase-counter [counter line]
-  (let [total (inc (:total counter))
-        sum (sum (:sum counter) line)]
-    {:total total :sum sum}))
-
-(defn solution [lines]
-  (reduce increase-counter {:total 0 :sum []} (map parse lines)))
-
 (defn from-binary [binary]
   (let [powers (map #(math/expt 2 %) (range))]
     (->> binary
@@ -25,13 +12,21 @@
          (map vector powers)
          (map #(if (true? (last %)) (first %) 0))
          (reduce +))))
-    ;; powers))
 
-;; pairs (map vector binary powers)
-;; numbers (map)
+(defn sum [total line]
+  (let [pairs (map vector (concat total (repeat 0)) line)]
+    (map (fn [[value bit]] (if (true? bit) (inc value) value)) pairs)))
 
-(defn provide-solution [solution file]
-  (let [{total :total sum :sum} (tools/with-open-file (str "src/day3/" file) solution)
+(defn increase-counter [counter line]
+  (let [total (inc (:total counter))
+        sum (sum (:sum counter) line)]
+    {:total total :sum sum}))
+
+(defn solution1 [lines]
+  (reduce increase-counter {:total 0 :sum []} (map parse lines)))
+
+(defn provide-solution1 [file]
+  (let [{total :total sum :sum} (tools/with-open-file (str "src/day3/" file) solution1)
         half (/ total 2)
         gamma_bin (map #(> half %) sum)
         epsilon_bin (map #(< half %) sum)
@@ -47,6 +42,6 @@
 
   (sum [1 2 3 4] (parse "1010001"))
 
-  (provide-solution solution "input-sample.txt")
+  (provide-solution1 "input-sample.txt")
 
-  (provide-solution solution "input.txt"))
+  (provide-solution1 "input.txt"))
