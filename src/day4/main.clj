d70f7a145fcb2b8bef05d5f2f8b8be73777453a1
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2021-12-09 21:15:02 +0100

    towards solution

--- src/day4/main.clj
+++ src/day4/main.clj
@@ -22,12 +22,35 @@
     {:numbers (map #(Integer/parseInt %) (str/split numbers #","))
      :boards (map load-board boards)}))
 
+(defn mark-number [draw]
+  (map #(replace {(:number draw) nil} %) (:board draw)))
+
+(defn wins? [board]
+  (letfn [(wins-lines? [b] (some? (some #(every? nil? %) b)))]
+    (or (wins-lines? board)
+        (wins-lines? (apply map list board)))))
+
+(defn score [board number])
+
 (defn solution1 [file]
-  (let [input (load-input file)]
-    input))
+  (let [input (load-input file)
+        numbers (:numbers input)
+        boards (:boards input)
+        draws (for [number numbers board boards] {:number number :board board})]
+    ;; draws))
+    (map mark-number  draws)))
 
 (comment
 
   (pp/pprint (solution1 "input-sample.txt"))
+
+  (replace {3 nil} [1 2 4 3])
+
+  (pp/pprint (load-input "input-sample.txt"))
+
+  (wins? [[1 3 4] [3 2 5]])
+  (wins? [[3 2 5] [nil nil nil]])
+  (wins? [[nil 2 5] [nil 1 3]])
+  (apply map list [[nil 2 5] [nil 1 3]])
 
   (load-board-line " 9 18 13 17  5"))
