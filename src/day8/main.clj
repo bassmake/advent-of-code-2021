ad03835249c29ff582387d02ef95b44c3f0b7b22
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2021-12-13 20:00:26 +0100

    day8 part1

--- src/day8/main.clj
+++ src/day8/main.clj
@@ -1,3 +1,32 @@
-(ns day8.main)
+(ns day8.main
+  (:require [tools.main :as t]
+            [clojure.string :as s]))
 
-()
+
+(defn parse-line [line]
+  (let [parts (s/split line #"\|")
+        patterns (s/split (s/trim (first parts)) #" ")
+        output (s/split (s/trim (last parts)) #" ")]
+    {:patterns patterns
+     :output output}))
+
+(defn solution1 [file]
+  (->> (s/split-lines (slurp (str "src/day8/" file)))
+       (map parse-line)
+       (map :output)
+       (flatten)
+       (map count)
+       (filter (fn [length] (some #{length} [2 3 4 7])))
+       (count)))
+
+
+
+(comment
+  
+  (solution1 "input-sample.txt")
+  (solution1 "input.txt")
+
+  (count "fcge")
+  (contains? [2 3 4 7] 7)
+
+  (parse-line "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"))
